151f1f0ef3f318ec56ab39ecb5b0f2101e010cc7
ar8006 <103987142+ar8006@users.noreply.github.com>
2022-04-19 12:22:32 +0530

    Add files via upload

---
+++ Switch.c
@@ -0,0 +1,41 @@
+// Program to create a simple calculator
+#include <stdio.h>
+
+int main() {
+    char operation;
+    int n1, n2;
+
+    printf("Enter an operator: ");
+    scanf("%c", &operation);
+    printf("Enter two operands: ");
+    scanf("%d %d",&n1, &n2);
+
+    switch(operation)
+    {
+        case '+':
+            printf("%d + %d = %d",n1, n2, n1+n2);
+            break;
+
+        case '-':
+            printf("%d - %d = %d",n1, n2, n1-n2);
+            break;
+
+        case '*':
+            printf("%d * %d = %d",n1, n2, n1*n2);
+            break;
+
+        case '/':
+            printf("%d / %d = %d",n1, n2, n1/n2);
+            break;
+        
+        case '%':
+            printf("%d",/*n1, n2,*/ n1%n2);
+            break;
+
+        
+        default:
+            printf("Error!");
+    }
+
+    return 0;
+}
